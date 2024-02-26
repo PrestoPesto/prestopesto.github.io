74cfb77e57f1da057814e5d3b59cc977f3af6d34
PrestoPesto <60044031+PrestoPesto@users.noreply.github.com>
2024-02-26 14:37:49 -0600

    Update fight.js

--- fight/fight.js
+++ fight/fight.js
@@ -13,21 +13,41 @@ let currentHealth = 10;
 let level = 1;
 let xp = 0;
 
-function spawnEnemy(color) {
-    enemyText.classList.remove("announceAnim");
-    enemyText.classList.remove("hidden");
-    void enemyText.offsetWidth;
-    enemyText.classList.add("announceAnim");
-    if (color == "c") {
-        enemyText.style.color = "cyan";
-        enemyText.innerHTML = "antC attacks!"
-    } else if (color == "m") {
-        enemyText.style.color = "magenta";
-        enemyText.innerHTML = "antM attacks!"
-    } else if (color == "y") {
-        enemyText.style.color = "yellow";
-        enemyText.innerHTML = "antY attacks!"
-    }
+let antColor;
+
+function spawnEnemy() {
+    ant.classList.remove("antSpawnAnim");
+    void ant.offsetWidth;
+    ant.classList.add("antSpawnAnim");
+    setTimeout(function() {
+        let antTypeRNG = Math.floor(Math.random() * 3);
+        if (antTypeRNG == 0) { 
+            antColor = "c";
+            ant.style.backgroundImage = "url(https://github.com/PrestoPesto/prestopesto.github.io/blob/main/fight/assets/antC.png?raw=true)";
+            enemyText.style.color = "cyan";
+            enemyText.innerHTML = "antC attacks!"
+        } else if (antTypeRNG == 1) {
+            antColor = "m";
+            ant.style.backgroundImage = "url(https://github.com/PrestoPesto/prestopesto.github.io/blob/main/fight/assets/antM.png?raw=true)";
+            enemyText.style.color = "magenta";
+            enemyText.innerHTML = "antM attacks!"
+        } else if (antTypeRNG == 2) {
+            antColor = "y";
+            ant.style.backgroundImage = "url(https://github.com/PrestoPesto/prestopesto.github.io/blob/main/fight/assets/antY.png?raw=true)";
+            enemyText.style.color = "yellow";
+            enemyText.innerHTML = "antY attacks!"
+        }
+        setTimeout(function() { 
+            enemyText.classList.remove("announceAnim");
+            enemyText.classList.remove("hidden");
+            void enemyText.offsetWidth;
+            enemyText.classList.add("announceAnim");
+        }, 1460);
+    }, 1000);
+    setTimeout(function() {
+        ant.classList.remove("antSpawnAnim");
+        ant.classList.add("antIdle");
+    }, 3000);
 }
 
 function updateHealth(dmg) {
@@ -49,8 +69,8 @@ function study() {
     ant.classList.add("antSpawnAnim");
 }
 
-studyButton.onclick = function() {study()};
+studyButton.onclick = function() {spawnEnemy();}
 
-spawnEnemy("m");
+spawnEnemy();
 updateHealth(0);
 updateLevel(0);
