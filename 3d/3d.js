aa2e92e2e284f44e067e67a168dd3d22b109a91e
EpicNecromancer <60044031+EpicNecromancer@users.noreply.github.com>
2024-04-02 21:50:29 -0500

    3D

--- 3d/3d.js
+++ 3d/3d.js
@@ -1,4 +1,6 @@
 let ground = document.getElementById("ground");
+let borders = document.getElementById("borders");
+let ouch = document.getElementById("ouch");
 
 document.body.addEventListener("keydown", function(e){keyDown[e.key] = true;  });
 document.body.addEventListener("keyup", function(e){keyDown[e.key] = false; });
@@ -10,38 +12,113 @@ let rotM = 0;
 let slide = 0;
 let isKeyDown = true;
 let maxRot = 0;
+let onGround = true;
+let air = 0;
+let airM = 0;
+let ouchPos = -20000;
+let isAlive = true;
 
 let keyDown = {};
 
 function move(e) {
-  if (keyDown["w"] && posM < 100) {
-    posM += 1;
+  if (keyDown["w"] && posM < 100 && onGround) {
+    posM++;
   }
   if (keyDown["s"] && posM >= 1) {
-    posM -= 1;
+    posM--;
   }
   pos += posM / 2;
+  ouchPos += posM / 2;
+
   if (pos >= 960) {
     pos = 720;
   }
-  if (posM > 0) {
+  if (posM > 0 && onGround) {
     posM -= 0.5;
+  }
+
+  if (ouchPos >= 0) {
+    ouchPos = -20000;
+  }
+
+  if (ouchPos >= -2400 && onGround) {
+    isAlive = false;
   }
 
   if (keyDown["d"]) {
     rotM += 0.5;
   }
+  if (keyDown["a"]) {
+    rotM -= 0.5;
+  }
   rot += rotM / 4;
 
+  if (rotM > 0) {
+    rotM -= 0.03;
+  }
+  if (rotM < 0) {
+    rotM += 0.03;
+  }
+
+  if (rot > 360) {
+    rot = 360;
+    rotM = 0;
+  }
+  if (rot < -360) {
+    rot = -360;
+    rotM = 0;
+  }
+
+  if (keyDown[" "] && onGround && isAlive) {
+    airM += 40;
+    onGround = false;
+  }
+
+  air += airM / 4;
+
+  if (air > 0 || !isAlive) {
+    airM--;
+    onGround = false;
+  } else {
+    onGround = true;
+  }
+
+  if (onGround) {
+    airM = 0;
+  }
+
   ground.style.transform = 
-    //"translateX(" + (-rot * 50) + "px) " + 
+    "scale(2) " +
+    "translateX(" + (-rot / 10) + "px) " + 
     "perspective(13em) " + 
-    "rotateX(40deg) " + 
-    //"rotateY(" + (rot / 5) + "rad) " + 
+    "rotateX(48deg) " + 
+    "rotateY(" + (rot / 900) + "rad) " + 
     "translateX(" + (-rot) + "px) " + 
-    //"translateZ(" + slide + "px) " + 
     "translateY(" + (-19280 + pos) + "px) " +
-    "scale(" + 1 - (posM / 1000) + ")";
+    "translateZ(" + -air + "px) " +
+    "scale(" + (1 - (posM / 6000)) + ")";
+
+  borders.style.transform = 
+    "scale(2) " +
+    "translateX(" + (-rot / 10) + "px) " + 
+    "perspective(13em) " + 
+    "rotateX(48deg) " + 
+    "rotateY(" + (rot / 900) + "rad) " + 
+    "translateX(" + (-rot) + "px) " + 
+    "translateY(" + (-19280 + pos) + "px) " +
+    "translateZ(" + -air + "px) " +
+    "scale(" + (1 - (posM / 6000)) + ")";
+
+  ouch.style.transform = 
+    "scale(2) " +
+    "translateX(" + (-rot / 10) + "px) " + 
+    "perspective(13em) " + 
+    "rotateX(48deg) " + 
+    "rotateY(" + (rot / 900) + "rad) " + 
+    "translateX(" + (-rot) + "px) " + 
+    "translateY(" + (ouchPos) + "px) " +
+    "translateZ(" + -air + "px) " +
+    "scale(" + (1 - (posM / 6000)) + ")";
 }
 
 setInterval(move, 10);
